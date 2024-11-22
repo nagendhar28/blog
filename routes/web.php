d4877fcfe77348419e018892144d49d05f593754
nagendhar <nagendhar.it@gmail.com>
2024-11-22 10:12:49 +0530

    v-4 completed

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login',[AuthController::class,'login']);
+Route::get('register',[AuthController::class,'register']);
+Route::get('forgot-password',[AuthController::class,'forgot']);
